f07f8b20c97efc430b309f8920a55f9b7bc7fc78
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-28 16:31:24 +0200

    MediatR Intro

---
+++ MediatR/DemoLibrary/Handlers/GetPersonListHendler.cs
@@ -0,0 +1,25 @@
+﻿using DemoLibrary.DataAccess;
+using DemoLibrary.Models;
+using DemoLibrary.Models.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Handlers
+{
+    public class GetPersonListHendler : IRequestHandler<GetPersonListQuery, List<PersonalModel>>
+    {
+        private readonly IDataAccess _data;
+        public GetPersonListHendler(IDataAccess data)
+        {
+            _data= data;
+        }
+        public Task<List<PersonalModel>> Handle(GetPersonListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_data.GetPeople());
+        }
+    }
+}
