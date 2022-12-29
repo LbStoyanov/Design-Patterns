523178e6f522dfbab39c4edbd41591a4edf5f11a
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-29 15:25:41 +0200

    MediatR Intro

---
+++ MediatR/DemoLibrary/Handlers/GetPersonByIdHendler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary.DataAccess;
+using DemoLibrary.Models;
+using DemoLibrary.Models.Queries;
+using MediatR;
+
+namespace DemoLibrary.Handlers
+{
+    public class GetPersonByIdHendler : IRequestHandler<GetPersonByIdQuery, PersonalModel>
+    {
+        private readonly IMediator _mediator;
+
+        public GetPersonByIdHendler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<PersonalModel> Handle(GetPersonByIdQuery request, CancellationToken cancellationToken)
+        {
+            var results = await _mediator.Send(new GetPersonListQuery());
+            var output = results.FirstOrDefault(x => x.Id == request.Id);
+
+            return output;
+        }
+    }
+}
---
+++ MediatR/DemoLibrary/Models/Queries/GetPersonByIdQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace DemoLibrary.Models.Queries
+{
+    //Shorter syntax
+    public record GetPersonByIdQuery(int Id) : IRequest<PersonalModel>;
+
+    //Class approach
+
+    //public class GetPersonByIdQuery : IRequest<PersonalModel>
+    //{
+    //    public GetPersonByIdQuery(int id) 
+    //    {
+    //        Id = id;
+    //    }
+    //    public int Id { get; set; }
+    //}
+}
